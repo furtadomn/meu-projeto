1fbd0c845853ccb498884808d8a78c9a2f25f8f3
furtadomn <mnfurtado@yahoo.com.br>
2021-11-29 15:38:10 -0300

    Eventos no React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './Evento';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
-      < HelloWorld />
-      < Frase />
-      < SayMyName nome="Marcella" />
-      < Pessoa 
-        nome="Marcella" 
-        idade="28" 
-        profissao="Desenvolvedora" 
-        foto="https://via.placeholder.com/150" />
-        < List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
     </div>
   );
 }
