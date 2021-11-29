a90ef8290f20ba9327b20e1ed46e4d54d90f3582
furtadomn <mnfurtado@yahoo.com.br>
2021-11-29 20:27:01 -0300

    Passar eventos por props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Form from './Form';
+import Evento from './Evento';
 
 function App() {
   return (
     <div className="App">
+      <Evento />
       <Form />
     </div>
   );
