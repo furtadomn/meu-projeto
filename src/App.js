2cf5e01b063d5b7cbed32263ecad3e6a04840d01
furtadomn <mnfurtado@yahoo.com.br>
2021-11-29 19:49:06 -0300

    useState na pratica

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Evento from './Evento';
 import Form from './Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento numero="1" />
-      <Evento numero="2" />
       <Form />
     </div>
   );
