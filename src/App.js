ecf62e9654eb4a73e45fdd8d9fc58f8d1f880541
furtadomn <mnfurtado@yahoo.com.br>
2021-11-29 12:32:32 -0300

    Inserindo CSS no React

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   return (
     <div className="App">
       < HelloWorld />
+      < Frase />
       < SayMyName nome="Marcella" />
       < Pessoa 
         nome="Marcella" 
